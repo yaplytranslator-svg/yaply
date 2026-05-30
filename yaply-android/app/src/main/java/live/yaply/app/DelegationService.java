e8ec6a72c07af54f6d0a09cdfc08664946388f5f
Piyush <yaplytranslator@gmail.com>
2026-05-30 17:59:33 +0530

    feat: add SHA26 fingerprint to assetlinks.json

---
+++ yaply-android/app/src/main/java/live/yaply/app/DelegationService.java
@@ -0,0 +1,14 @@
+package live.yaply.app;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
